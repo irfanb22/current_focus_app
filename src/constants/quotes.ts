bfc9944fec830fdecbaec5f1c150745ebcbee2c7
irfanb22 <77904897+irfanb22@users.noreply.github.com>
2025-07-10 17:06:49 -0500

    Create quotes constants file

---
+++ src/constants/quotes.ts
@@ -0,0 +1,27 @@
+export const quotes = [
+  {
+    text: "Task anxiety is a house of cards. It falls apart the moment you start",
+    author: "Irfan Bhanji",
+    screen: "unpleasant-5min"
+  },
+  {
+    text: "There's no such thing as *later*. It's just another word for *never*.",
+    author: "Sahil Bloom", 
+    screen: "pleasant-timer"
+  },
+  {
+    text: "Doing things is energizing, wasting time is depressing. You don't need that much 'rest'.",
+    author: "Nabeel S. Qureshi",
+    screen: "pleasant-timer"
+  },
+  {
+    text: "If you do the most important thing first each day, then you'll always get something important done",
+    author: "James Clear",
+    screen: "completion"
+  },
+  {
+    text: "Never sacrifice momentum. I might know a better path, but if we've got a lot of momentum, if everyone's united and they're marching together and the path is O.K., just go with the flow.",
+    author: "Ben Chestnut",
+    screen: "completion"
+  }
+];
