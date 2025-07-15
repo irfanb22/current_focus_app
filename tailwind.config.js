ecbc85c71c312324fe1766071d4907c8fd589d2a
irfanb22 <77904897+irfanb22@users.noreply.github.com>
2025-07-14 19:25:58 -0500

    Update background color in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       colors: {
         primary: '#B0D9E8',
-        background: '#F9FAFB',
+        background: '#F3F4F6',
         'background-alt': '#EDEFF1',
         success: '#D6F5E3',
         'text-primary': '#2E3A45',
